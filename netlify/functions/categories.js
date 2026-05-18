38489956383e123e71a361b702402aaf1bb35084
douqian <226854154@qq.com>
2026-05-18 15:48:33 +0800

    恢复Netlify配置文件和函数，删除误入的vercel.json

---
+++ netlify/functions/categories.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async () => {
+  try {
+    const questionsPath = path.join(__dirname, '..', '..', 'public', 'questions.json');
+    const questionsData = fs.readFileSync(questionsPath, 'utf8');
+    const questions = JSON.parse(questionsData);
+    return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(Object.keys(questions)) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: '获取分类失败' }) };
+  }
+};
